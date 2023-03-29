265612a8779f3e9195c5b128960edbf6ed12fdc0
suprajaarthi <supr17318.cs@rmkec.ac.in>
2023-03-29 10:56:00 +0530

    2

--- multival.js
+++ multival.js
@@ -14,45 +14,6 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 
-const paragraph = document.querySelector('p');
-
-firebase.database().ref('users').on('child_added', (snapshot) => {
-  const values = snapshot.val();
-
-  // paragraph.textContent += `${values.dropdown1} - ${values.dropdown2} - ${values.dropdown3}\n`;
-});
-
-
-var hotel-form= document.getElementById('my-form');
-
-hotel-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the default form submission behavior
-
-  
-  var checkin = document.getElementById("checkin").value;
-  var checkout     = document.getElementById("checkout").value;
-  
- 
- console.log(checkin);
-  console.log(checkout);
-  // location.href = "availabletrain.html";
-  // Store the form data in Firebase
-  database.ref('users').push({
-    checkout : checkout, 
-    checkin  : checkin
-
-  });
-});
-
-
-
-
-
-
-
-
-
-
 var form = document.getElementById('my-form');
 
 form.addEventListener('submit', function(event) {
@@ -66,16 +27,12 @@ form.addEventListener('submit', function(event) {
  var dropdown2 = form.elements['dropdown2 '].value;
  var dropdown3 = form.elements['dropdown3'].value;
 
-  // const dropdown1 = document.querySelector('#dropdown1');
-  // const dropdown2 = document.querySelector('#dropdown2');
-  // const dropdown3 = document.querySelector('#dropdown3');
 
-  var passenger = document.getElementById("passenger").value;
-  var price     = document.getElementById("price").value;
-  
- 
+ var passenger = form.elements['passenger'].value;
+var price = form.elements['price'].value;
+
  console.log(passenger);
-  location.href = "availabletrain.html";
+  // location.href = "availabletrain.html";
   // Store the form data in Firebase
   database.ref('users').push({
     name: name,
@@ -87,7 +44,6 @@ form.addEventListener('submit', function(event) {
     dropdown1: dropdown1,
     dropdown2: dropdown2,
     dropdown3: dropdown3
-
 
   });
 });
